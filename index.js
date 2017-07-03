d8c057a0c8a435a78eb4e9d627f512a5cc4e2630
comp500 <comp500@users.noreply.github.com>
2017-07-03 16:29:58 +0100

    add eris to code

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// require bots
 const Skyweb = require('skyweb');
+const Eris = require("eris");
+const creds = require('../creds.json');
+
 var skyweb = new Skyweb();
-var creds = require('../creds.json');
+var discord = new Eris(creds.discordtoken);
+var skywebReady = false;
+var discordReady = false;
+
 skyweb.login(creds.skypeuser, creds.skypepass).then(function (skypeAccount) {
-	console.log('Skyweb is initialized now');
+	console.log('Skyweb ready!');
+	skywebReady = true;
+	console.log('Your contacts : ' + JSON.stringify(skyweb.contactsService.contacts, null, 2));
 }).catch(function (e) {
 	console.error(e);
 });
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Discord ready!"); // Log "Ready!"
+	discordReady = true;
+});
+
