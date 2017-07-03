c9cb796d37ffe329c49b212bae231a7ecabf3b44
comp500 <comp500@users.noreply.github.com>
2017-07-03 17:04:09 +0100

    add callbacks for messages

--- index.js
+++ index.js
@@ -7,17 +7,72 @@ var skyweb = new Skyweb();
 var discord = new Eris(creds.discordtoken);
 var skywebReady = false;
 var discordReady = false;
+var skypeQueue = [];
+var discordQueue = [];
+var discordChannel;
+var lastID = null;
 
 skyweb.login(creds.skypeuser, creds.skypepass).then(function (skypeAccount) {
 	console.log('Skyweb ready!');
 	skywebReady = true;
-	console.log('Your contacts : ' + JSON.stringify(skyweb.contactsService.contacts, null, 2));
+	if (skypeQueue.length > 0) {
+		for (var i = 0; i < skypeQueue.length; i++) {
+			skyweb.sendMessage(skypeQueue[i].id, skypeQueue[i].message);
+		}
+	}
 }).catch(function (e) {
 	console.error(e);
 });
 
-bot.on("ready", () => { // When the bot is ready
-    console.log("Discord ready!"); // Log "Ready!"
-	discordReady = true;
+skyweb.messagesCallback = (messages) => {
+    messages.forEach((message) => {
+        if (message.resource.from.indexOf(creds.skypeuser) === -1 && message.resource.messagetype !== 'Control/Typing' && message.resource.messagetype !== 'Control/ClearTyping') {
+			var conversationLink = message.resource.conversationLink;
+            var conversationId = conversationLink.substring(conversationLink.lastIndexOf('/') + 1);
+			if (discordReady) {
+				discordChannel.createMessage("[" + conversationId + "] " + message.resource.content);
+			} else {
+				discordQueue.push("[" + conversationId + "] " + message.resource.content);
+			}
+        }
+    });
+};
+
+discord.on("ready", () => {
+	discord.getDMChannel(creds.discorduser).then(function (channel) {
+		console.log("Discord ready!");
+		discordChannel = channel;
+		discord.on("messageCreate", function (msg) {
+			var matches = /\s*\[(\S+)\]\s*(\S*)/i.exec(msg.content);
+			var message = null;
+			if (matches == null) {
+				if (lastID == null) {
+					discordChannel.createMessage("Message failed to send. Ensure you specify channel ID.");
+				} else {
+					message = msg.content;
+				}
+			} else {
+				lastID = matches[1];
+				message = matches[2];
+			}
+			if (message) {
+				if (skypeReady) {
+					skyweb.sendMessage(lastID, message);
+				} else {
+					skypeQueue.push({
+						id: lastID,
+						message: message
+					});
+				}
+			}
+		});
+		discordReady = true;
+		if (discordQueue.length > 0) {
+			for (var i = 0; i < discordQueue.length; i++) {
+				discordChannel.createMessage(discordQueue[i]);
+			}
+		}
+	});
 });
 
+discord.connect();
